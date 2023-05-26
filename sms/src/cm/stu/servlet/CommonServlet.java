faa352ca81bcdf54cd24a713207d9c765b319506
RuihaoWei95 <ruihw95@gmail.com>
2023-05-26 16:31:55 -0700

    add jump back to main page
    add jump back to main page for all users

---
+++ sms/src/cm/stu/servlet/CommonServlet.java
@@ -0,0 +1,22 @@
+package cm.stu.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/common.action")
+public class CommonServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+
+        if(action.equals("index")){
+            //jump to main page
+            req.setAttribute("mainRight", "blank.jsp");
+            req.getRequestDispatcher("main.jsp").forward(req, resp);
+        }
+    }
+}
