c7bd400b773d882ab25da73c6fb384680d71815c
RuihaoWei95 <ruihw95@gmail.com>
2023-05-27 18:15:51 -0700

    show all students for teacher

---
+++ sms/src/cm/stu/dao/Deal.java
@@ -0,0 +1,44 @@
+package cm.stu.dao;
+
+import cm.stu.bean.Person;
+import cm.stu.util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deal {
+    public static List<Person> getAllPerson(String sql) {
+        Connection connection = ConnectionFactory.getConnection();
+        PreparedStatement pre = null;
+        ResultSet res = null;
+        List<Person> list = new ArrayList<>();
+        try{
+            pre = connection.prepareStatement(sql);
+            res = pre.executeQuery();
+            while(res.next()){
+                Person newPerson = new Person();
+                //`userAccount``userName``userSex``userBirthday``userIdCard``userPassword``userIdentify``userOtherName`
+                newPerson.setUserAccount(res.getString("userAccount"));
+                newPerson.setUserName(res.getString("userName"));
+                newPerson.setUserSex(res.getString("userSex"));
+                newPerson.setUserBirthday(res.getString("userBirthday"));
+                newPerson.setUserIdCard(res.getString("userIdCard"));
+                newPerson.setUserPassword(res.getString("userPassword"));
+                newPerson.setUserIdentify(res.getInt("userIdentify"));
+                newPerson.setUserOtherName(res.getString("userOtherName"));
+                list.add(newPerson);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+
+        }
+        return list;
+
+    }
+}
---
+++ sms/src/cm/stu/dao/TeacherDao.java
@@ -0,0 +1,7 @@
+package cm.stu.dao;
+
+import java.util.List;
+
+public interface TeacherDao {
+    List AllStudentsByTeacherAccount(String userAccount);
+}
