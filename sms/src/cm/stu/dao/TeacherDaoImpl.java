178d78f4c17d6c869feff06ac103c3fd14d796ca
RuihaoWei95 <ruihw95@gmail.com>
2023-05-28 15:28:37 -0700

    clean up codes

--- sms/src/cm/stu/dao/TeacherDaoImpl.java
+++ sms/src/cm/stu/dao/TeacherDaoImpl.java
@@ -8,30 +8,6 @@ import java.util.List;
 public class TeacherDaoImpl implements TeacherDao{
 
     @Override
-    public List AllStudentsByTeacherAccount(String userAccount) {
-        String sql = "Select * FROM person WHERE userAccount IN (SELECT userAccount FROM userteacher WHERE teacherAccount='"+userAccount+"')";
-        return Deal.getAllPerson(sql);
-    }
-
-    @Override
-    public List<Task> getAllMyTask(String userAccount) {
-        String sql = "SELECT * FROM task WHERE teacherAccount = '" + userAccount+"'";
-        return Deal.getAllTask(sql);
-    }
-
-    @Override
-    public List<StudentAnswer> getTaskDetail(String taskAccount) {
-        String sql ="SELECT s.*,p.`userName`,`taskName` FROM `studenttask` s  INNER JOIN person p ON s.`studentAccount`=p.`userAccount` INNER JOIN task t ON s.`taskAccount`=t.taskAccount WHERE s.`taskAccount`='"+taskAccount+"'";
-        return Deal.getTaskDetail(sql);
-    }
-
-    @Override
-    public List<Course> getAllMyCourse(String userAccount) {
-        String sql = "SELECT c.* FROM course c WHERE c.CourseId IN (SELECT courseId FROM `teachercourse` WHERE userAccount='"+userAccount+"')";
-        return Deal.getAllCourse(sql);
-    }
-
-    @Override
     public List<CourseGrade> getAllMyCourseGrade(String userAccount) {
         String sql = "SELECT c.courseName, c.courseTime, c.courseId, MIN(sc.score) as low, AVG(sc.score) as mean, MAX(sc.score) as high FROM course c LEFT JOIN teachercourse tc ON c.CourseId = tc.courseID LEFT JOIN studentcourse sc ON tc.courseID = sc.courseId GROUP BY c.courseID, tc.userAccount HAVING userAccount = '"+ userAccount + "'";
         return Deal.getAllCourseGrade(sql);
--- sms/src/cm/stu/service/TeacherServiceImpl.java
+++ sms/src/cm/stu/service/TeacherServiceImpl.java
@@ -8,25 +8,6 @@ import java.util.List;
 
 public class TeacherServiceImpl implements TeacherService{
     TeacherDao td = new TeacherDaoImpl();
-    @Override
-    public List AllStudentsByTeacherAccount(String userAccount) {
-        return td.AllStudentsByTeacherAccount(userAccount);
-    }
-
-    @Override
-    public List<Task> getAllMyTask(String userAccount) {
-        return td.getAllMyTask(userAccount);
-    }
-
-    @Override
-    public List<StudentAnswer> getTaskDetail(String taskAccount) {
-        return td.getTaskDetail(taskAccount);
-    }
-
-    @Override
-    public List<Course> getAllMyCourse(String userAccount) {
-        return td.getAllMyCourse(userAccount);
-    }
 
     @Override
     public List<CourseGrade> getAllMyCourseGrade(String userAccount) {
